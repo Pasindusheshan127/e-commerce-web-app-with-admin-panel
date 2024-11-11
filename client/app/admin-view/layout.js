0ba70ad8f22ec23ace8ab75d564b9275f361eb88
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-11-11 11:39:06 +0530

    Login page and registration page are created with layout in auth folder

---
+++ client/app/admin-view/layout.js
@@ -0,0 +1,13 @@
+const AdminLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen w-full">
+      {/* admin sliebar */}
+      <div className="flex flex-1 flex-col">
+        {/* admin header */}
+        <main className="flex flex-1 bg-muted/40 p-4 md:p-6">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
