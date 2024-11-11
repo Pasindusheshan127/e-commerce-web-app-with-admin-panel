4e27f94e79a4486233ec792887e61824f367fe3a
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-11-11 12:43:35 +0530

    Dashboard page , order page , feature page and product page have been created with layout in admin-views folder

--- client/app/admin-view/layout.js
+++ client/app/admin-view/layout.js
@@ -1,9 +1,14 @@
+import AdminHeader from "@/components/Header";
+import AdminSidebar from "@/components/Sidebar";
+
 const AdminLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen w-full">
       {/* admin sliebar */}
+      <AdminSidebar />
       <div className="flex flex-1 flex-col">
         {/* admin header */}
+        <AdminHeader />
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6">{children}</main>
       </div>
     </div>
