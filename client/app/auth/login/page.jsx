c269854d53a727e626747009397cc2b5d5b6756f
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-11-13 13:09:04 +0530

    login page and register page are created

--- client/app/auth/login/page.jsx
+++ client/app/auth/login/page.jsx
@@ -1,13 +1,47 @@
+"use client";
+import CommonForm from "@/components/common/CommonForm";
+import { loginFormControls } from "@/config";
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 
-const page = () => {
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // TODO: send form data to the server for registration
+    console.log(formData);
+  };
+
   return (
-    <div>
-      page
-      <Link href="/auth/register">Home</Link>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <div className="mt-2 flex items-center justify-center">
+          <span className="mr-2">Don't have an account?</span>
+          <Link href="/auth/register">
+            <p className="font-medium text-primary hover:underline-offset-0">
+              Register
+            </p>
+          </Link>
+        </div>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
 
-export default page;
+export default Login;
--- client/app/auth/register/page.jsx
+++ client/app/auth/register/page.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+import CommonForm from "@/components/common/CommonForm";
+import { registerFormControls } from "@/config";
+import Link from "next/link";
+import { useState } from "react";
 
-const page = () => {
-  return <div>page</div>;
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
 };
 
-export default page;
+const Register = () => {
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // TODO: send form data to the server for registration
+    console.log(formData);
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <div className="mt-2 flex items-center justify-center">
+          <span className="mr-2">Already have an account?</span>
+          <Link href="/auth/login">
+            <p className="font-medium text-primary hover:underline-offset-0">
+              Login
+            </p>
+          </Link>
+        </div>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default Register;
