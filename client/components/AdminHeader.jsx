e8494c5022a14770bf2361841be04a65c8f9d949
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-11-20 11:59:19 +0530

    AdminHeader component is created

--- client/components/AdminHeader.jsx
+++ client/components/AdminHeader.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import { AlignJustify, LogOut } from "lucide-react";
+import { Button } from "./ui/button";
 
 const AdminHeader = () => {
-  return <div>Admin</div>;
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button className="lg:hidden sm:block bg-black text-white">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          // onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow bg-black text-white"
+        >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
